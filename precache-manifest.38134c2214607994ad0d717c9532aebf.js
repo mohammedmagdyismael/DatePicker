7c83db51cead0dcf2505f4bc9f91fa29be143a12
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-13 20:05:19 +0200

    Updates

---
+++ precache-manifest.38134c2214607994ad0d717c9532aebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10276455218db431bf5d36cd176b7cf1",
+    "url": "/DatePicker/index.html"
+  },
+  {
+    "revision": "2bbcafc19817df79560e",
+    "url": "/DatePicker/static/css/main.9aa4e6d0.chunk.css"
+  },
+  {
+    "revision": "810aed7ff110419f8c95",
+    "url": "/DatePicker/static/js/2.06ce8623.chunk.js"
+  },
+  {
+    "revision": "2bbcafc19817df79560e",
+    "url": "/DatePicker/static/js/main.ce8e06bf.chunk.js"
+  },
+  {
+    "revision": "477746ee975a21493b6e",
+    "url": "/DatePicker/static/js/runtime-main.69d9f1ee.js"
+  }
+]);
